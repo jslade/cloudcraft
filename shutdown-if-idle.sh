2beae8bb78eac5b1481aec00444b9197b279e5a8
N Bailey <nikobelia@gmail.com>
2016-06-02 19:39:58 +0100

    Unlovely but functional shutdown bash script...

--- shutdown-if-idle.sh
+++ shutdown-if-idle.sh
@@ -4,27 +4,29 @@ MINECRAFT_PORT=25565
 MINECRAFT_SERVICE='minecraft'
 
 function stop-server {
-  echo "Here we stop the server"
-  echo "We'll configure service user creds on the server"
-  echo "Then use Boto to stop the instance from the hypervisor"
+  echo "Users logged on to the server are: `users`"
+  echo "Stopping the server at `date`"
+  instance=`aws ec2 describe-instances --filters Name=tag:role,Values=minecraft --query Reservations[].Instances[].[InstanceId] | grep -o '".*"' | sed 's/"//g'`
+  aws ec2 stop-instances --instance-ids $instance
 }
 
 # listen for network connections on Minecraft's port.
-command="ss -t sport == ${MINECRAFT_PORT}"
 counter=0
 
 while [[ "${counter}" -lt 10 ]] ; do
+  command="/usr/sbin/ss -t sport == ${MINECRAFT_PORT}"
   conn=`$command`
-
+  echo ${conn}
   if [[ ${conn} == *"ESTAB"* ]] ; then
     echo "Minecraft is running"
     exit 0
   else
-    echo "Minecraft has no connected users, now let's check for logged in users."
+    echo "conn is $conn"
+    echo "Minecraft has no connected users at `date`, now let's check for logged in users."
   fi
 
   sessions=`users`
-  echo "Logged in users are: $sessions"
+  echo "Logged in users at `date` are: $sessions"
 
   if [ "${sessions}" == '' ] ; then
     counter=$((counter+1))
@@ -34,9 +36,11 @@ while [[ "${counter}" -lt 10 ]] ; do
     exit 0
   fi
 
-  sleep 180
+  sleep 60
 
 done
 
-instance=`aws ec2 describe-instances --filters Name=tag:role,Values=minecraft --query Reservations[].Instances[].[InstanceId] | grep -o '".*"' | sed 's/"//g'`
-aws ec2 stop-instances --instance-ids $instance
+if [[ "${counter}" == 10 ]] ; do
+  stop-server
+done
+
